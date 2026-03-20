e73a9d61f1b7a8c8b71f259d3155d4855c61f988
Aehsun Hafeez <73653865+Aehsun-Hafeez@users.noreply.github.com>
2026-03-20 23:21:02 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1234;
+let accountEmail = "aehsun.hafeez@gmail.com";
+var accountPassword = "2304";
+accountCity = "Lahore";
+let accountState;
+
+console.log(accountId);
+accountEmail = "bareera@gmail.com";
+accountPassword = "1407";
+accountCity = "Sialkot";
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
